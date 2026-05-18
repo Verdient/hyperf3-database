2a7a07683430049e8d9e2fcdd2802a25ef0523dc
Verdient <fagui.zhu@foxmail.com>
2026-05-18 16:13:40 +0800

    fix: 修复找不到映射类的问题

--- generate-model-comment.php
+++ generate-model-comment.php
@@ -6,7 +6,7 @@ use Hyperf\Support\Composer;
 use Verdient\Hyperf3\Database\ColumnManager;
 use Verdient\Hyperf3\Database\Command\CommentGenerator\BuilderGenerator;
 use Verdient\Hyperf3\Database\Command\CommentGenerator\ModelCommentGenerator;
-
+use Verdient\Hyperf3\Di\ClassLoader;
 
 if (empty($argv[1]) || empty($argv[2] || empty($argv[3] || empty($argv[4]) || empty($argv[5])))) {
     return;
@@ -14,11 +14,17 @@ if (empty($argv[1]) || empty($argv[2] || empty($argv[3] || empty($argv[4]) || em
 
 require $argv[1] . '/vendor/autoload.php';
 
+define('BASE_PATH', $argv[1]);
+
 $class = $argv[2];
 
 $path = $argv[3];
 
-Composer::getLoader()->addClassMap([$class => $path]);
+$classMap = ClassLoader::getClassMap();
+
+if (!empty($classMap)) {
+    Composer::getLoader()->addClassMap($classMap);
+}
 
 if (!empty($argv[5])) {
     $reflectionProperty = new ReflectionProperty(ColumnManager::class, 'columns');
